40b267788ea04b72d84d1beb6fae5725da940f74
Oliver Garcia <ogarci5@gmail.com>
2024-05-23 16:01:57 -0500

    Add specific functions to handle the drawing on box labels on tables. Ensures that each table structure label is bounded by the table it is closest to. For each table row, sets the boundaries to the full width. For each table column, sets the boundaries to the full height. Allows the snapping to the closest row or column. Allows both snapping for spanning cells.

--- image_labelling_tool/static/labelling_tool/box_label.ts
+++ image_labelling_tool/static/labelling_tool/box_label.ts
@@ -50,16 +50,26 @@ module labelling_tool {
         return new AABox(lower, upper);
     }
 
+    interface TableStructure {
+        bbox: AABox;
+        columns: AABox[];
+        entity: AbstractLabelEntity<AbstractLabelModel>;
+        label: string;
+        rows: AABox[];
+        threshold: Vector2;
+    }
 
     /*
     Box label entity
      */
     export class BoxLabelEntity extends AbstractLabelEntity<BoxLabelModel> {
         _rect: any;
+        _table: TableStructure|null;
 
 
         constructor(view: RootLabelView, model: BoxLabelModel) {
             super(view, model);
+            this._table = null;
         }
 
 
@@ -160,6 +170,54 @@ module labelling_tool {
 
         distance_to_point(point: Vector2): number {
             return BoxLabel_box(this.model).distance_to(point);
+        }
+
+        is_table_structure_label(label?: string): boolean {
+            const label_class = label ?? this?.model?.label_class;
+            if (label_class === null || label_class === undefined) return false;
+            const table_structure_classes = [
+                "table_column",
+                "table_column_header",
+                "table_projected_row_header",
+                "table_row",
+                "table_spanning_cell",
+            ]
+            return table_structure_classes.indexOf(label_class) !== -1;
+        }
+
+        get_parent_table(): TableStructure|null {
+            if (this._table !== null) return this._table;
+            if (!this.is_table_structure_label()) return null;
+            const entity_id = this.get_entity_id();
+
+            const entities: AbstractLabelEntity<AbstractLabelModel>[] = this.root_view.get_entities();
+            const table_entities = entities.filter((e) => e.model.label_class === "table");
+            table_entities.forEach((entity) => {
+                if (entity.distance_to_point(this.compute_centroid()) < 10) {
+                    const bbox = entity.compute_bounding_box();
+                    const columns = entities.filter((e) => {
+                        return entity_id !== e.get_entity_id() && e.model.label_class === "table_column" && bbox.contains_point(e.compute_bounding_box().centre());
+                    }).map((e) => e.compute_bounding_box());
+                    const rows = entities.filter((e) => {
+                        return entity_id !== e.get_entity_id() && e.model.label_class === "table_row" && bbox.contains_point(e.compute_bounding_box().centre());
+                    }).map((e) => e.compute_bounding_box());
+
+                    this._table = {
+                        bbox,
+                        columns,
+                        entity,
+                        label: this.model.label_class,
+                        rows,
+                        threshold: {
+                            x: Math.max(10, Math.ceil((bbox.upper.x - bbox.lower.x) / 100)),
+                            y: Math.max(10, Math.ceil((bbox.upper.y - bbox.lower.y) / 100)),
+                        }
+                    };
+                    return this._table;
+                }
+            });
+
+            return null;
         }
     }
 
@@ -268,6 +326,7 @@ module labelling_tool {
                 if (this._start_point !== null) {
                     if (this._current_point !== null) {
                         box = AABox_from_points([this._start_point, this._current_point]);
+                        box = this.get_table_box(this._start_point, this._current_point) ?? box;
                     }
                     else {
                         box = new AABox(this._start_point, this._start_point);
@@ -278,5 +337,67 @@ module labelling_tool {
                 this.entity.update();
             }
         };
+
+        get_table_box(_start: Vector2, _current: Vector2): AABox|null {
+            const table = this.entity.get_parent_table();
+            if (table === null) return null;
+
+            const bbox = table.bbox;
+            const start = bbox.closest_point_to(_start);
+            const current = bbox.closest_point_to(_current);
+            let box: AABox = null;
+
+            if (table.label === "table_row" || table.label === "table_column_header" || table.label === "table_projected_row_header") {
+                // full width
+                start.x = bbox.closest_boundary_to(start).x;
+                current.x = start.x === bbox.lower.x ? bbox.upper.x : bbox.lower.x;
+
+                this._snap_to_table_boundary(start, current, table, "y");
+                this._snap_to_row_boundary(start, current, table);
+            } else if (table.label === "table_column") {
+                // full height
+                start.y = bbox.closest_boundary_to(start).y;
+                current.y = start.y === bbox.lower.y ? bbox.upper.y : bbox.lower.y;
+
+                this._snap_to_table_boundary(start, current, table, "x");
+                this._snap_to_column_boundary(start, current, table);
+            // spanning cell
+            } else {
+                this._snap_to_table_boundary(start, current, table);
+                this._snap_to_column_boundary(start, current, table);
+                this._snap_to_row_boundary(start, current, table);
+            }
+
+            this._start_point = start;
+            box = AABox_from_points([start, current]);
+
+            return box;
+        }
+
+        _snap_to_table_boundary(start:Vector2, current:Vector2, table:TableStructure, type?: "x"|"y") {
+            if (type === undefined || type === "x") {
+                if (table.bbox.distance_to(start) < table.threshold.x) start.x = table.bbox.closest_point_to(start).x;
+                if (table.bbox.distance_to(current) < table.threshold.x) current.x = table.bbox.closest_point_to(current).x;
+            }
+            if (type === undefined || type === "y") {
+                if (table.bbox.distance_to(start) < table.threshold.y) start.y = table.bbox.closest_point_to(start).y;
+                if (table.bbox.distance_to(current) < table.threshold.y) current.y = table.bbox.closest_point_to(current).y;
+            }
+        }
+
+        // can break with below if matching multiple columns or rows
+        _snap_to_column_boundary(start:Vector2, current:Vector2, table:TableStructure) {
+            table.columns.forEach((col) => {
+               if (col.distance_to(start) < table.threshold.x) start.x = col.closest_boundary_to(start).x;
+               if (col.distance_to(current) < table.threshold.x) current.x = col.closest_boundary_to(current).x;
+            });
+        }
+
+        _snap_to_row_boundary(start:Vector2, current:Vector2, table:TableStructure) {
+            table.rows.forEach((row) => {
+               if (row.distance_to(start) < table.threshold.y) start.y = row.closest_boundary_to(start).y;
+               if (row.distance_to(current) < table.threshold.y) current.y = row.closest_boundary_to(current).y;
+            });
+        }
     }
 }
